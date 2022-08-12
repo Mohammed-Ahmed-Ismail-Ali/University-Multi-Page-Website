4529a069909eae449be8a5ccb0107b38869a4203
Mohamed <105673489+Mohammed-Ahmed-Ismail-Ali@users.noreply.github.com>
2022-08-12 05:46:07 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// Toggle Menu
+const navLinks = document.getElementById("navLinks");
+
+function openMenu() {
+  navLinks.style.right = "0";
+}
+function hideMenu() {
+  navLinks.style.right = "-200px";
+}
